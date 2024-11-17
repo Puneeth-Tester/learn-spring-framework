0c05edb308eea0124b26aa31dfb9f8f979049598
Puneeth-Tester <emailtopuneeth@gmail.com>
2024-11-17 15:23:31 +0530

    Without Dependency Injection

---
+++ src/main/java/com/spring/core/di/Client.java
@@ -0,0 +1,14 @@
+package com.spring.core.di;
+
+public class Client {
+
+    public static void main(String[] args) {
+
+        String message = "Hi, good morning hava a nice day!.";
+
+        SMSService smsService = new SMSService();
+        EmailService emailService = new EmailService();
+        MessageSender messageSender = new MessageSender(emailService);
+        messageSender.sendMessage(message);
+    }
+}
