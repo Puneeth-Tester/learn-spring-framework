dbdf74cd1b8bc0df6e224c7109708fb04a132931
Puneeth-Tester <emailtopuneeth@gmail.com>
2024-11-17 16:09:35 +0530

    Multiple Constructor Based DI

--- src/main/java/com/spring/core/di/MessageSender.java
+++ src/main/java/com/spring/core/di/MessageSender.java
@@ -9,12 +9,23 @@ public class MessageSender {
 
     private MessageService messageService;
 
-    @Autowired
+    private MessageService smsService;
+
     public MessageSender(@Qualifier("emailService") MessageService messageService) {
         this.messageService = messageService;
+        System.out.println("Constructor based DI");
+    }
+
+    @Autowired
+    public MessageSender(@Qualifier("emailService") MessageService messageService,
+                         @Qualifier("smsService") MessageService smsService) {
+        this.messageService = messageService;
+        this.smsService = smsService;
+        System.out.println("Two Parameter Constructor based DI");
     }
 
     public void sendMessage(String message){
         this.messageService.sendMessage(message);
+        this.smsService.sendMessage(message);
     }
 }
