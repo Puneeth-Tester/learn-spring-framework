a29eae2aaffe64076d727b8894d177cf098d8a5b
Puneeth-Tester <emailtopuneeth@gmail.com>
2024-12-02 23:41:29 +0530

    @Scope

---
+++ src/main/java/com/spring/core/scopeAnnotation/Mobile.java
@@ -0,0 +1,20 @@
+package com.spring.core.scopeAnnotation;
+
+public class Mobile extends Device {
+    private int batteryCapacity;
+    public Mobile() {
+        super();
+    }
+
+    public Mobile(String name, double price) {
+        super(name, price);
+    }
+
+    public int getBatteryCapacity() {
+        return batteryCapacity;
+    }
+
+    public void setBatteryCapacity(int capacity) {
+        this.batteryCapacity = capacity;
+    }
+}
